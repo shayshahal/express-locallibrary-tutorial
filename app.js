7dc2016ff6bc1ba7e3ed63cc02fb54c1cefaa235
shayshahal <shay840@gmail.com>
2023-05-15 10:32:25 +0300

    Added files and changes required for deployment

--- app.js
+++ app.js
@@ -1,76 +1,79 @@
-const createError = require('http-errors');
-const express = require('express');
-const path = require('path');
-const mongoose = require('mongoose');
-const cookieParser = require('cookie-parser');
-const logger = require('morgan');
-const helmet = require('helmet');
+const createError = require("http-errors");
+const express = require("express");
+const path = require("path");
+const cookieParser = require("cookie-parser");
+const logger = require("morgan");
 
-const compression = require('compression');
-const expressLayouts = require('express-ejs-layouts');
+const indexRouter = require("./routes/index");
+const usersRouter = require("./routes/users");
+const catalogRouter = require("./routes/catalog"); // Import routes for "catalog" area of site
+
+const compression = require("compression");
+const helmet = require("helmet");
+
 const app = express();
+
 // Set up rate limiter: maximum of twenty requests per minute
-const RateLimit = require('express-rate-limit');
+const RateLimit = require("express-rate-limit");
 const limiter = RateLimit({
-	windowMs: 1 * 60 * 1000, // 1 minute
-	max: 20,
+  windowMs: 1 * 10 * 1000, // 10 seconds
+  max: 10,
 });
 // Apply rate limiter to all requests
 app.use(limiter);
 
-mongoose.set('strictQuery', false);
+// Set up mongoose connection
+const mongoose = require("mongoose");
+mongoose.set("strictQuery", false);
+
 const dev_db_url =
-	'mongodb+srv://cooluser:coolpassword@cluster0.lz91hw2.mongodb.net/local_library?retryWrites=true&w=majority';
+  "mongodb+srv://shay:<password>@cluster0.acy88nl.mongodb.net/?retryWrites=true&w=majority";
 const mongoDB = process.env.MONGODB_URI || dev_db_url;
 
-main().catch((err) => console.error(err));
+main().catch((err) => console.log(err));
 async function main() {
-	await mongoose.connect(mongoDB);
+  await mongoose.connect(mongoDB);
 }
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'ejs');
 
-// Add helmet to the middleware chain.
-// Set CSP headers to allow our Bootstrap and Jquery to be served
-app.use(
-	helmet.contentSecurityPolicy({
-		directives: {
-			'script-src': ["'self'", 'code.jquery.com', 'cdn.jsdelivr.net'],
-		},
-	})
-);
-app.use(compression);
-app.use(logger('dev'));
+// view engine setup
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "pug");
+
+app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(expressLayouts);
 
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(
+  helmet.contentSecurityPolicy({
+    directives: {
+      "script-src": ["'self'", "code.jquery.com", "cdn.jsdelivr.net"],
+    },
+  })
+);
 
-const indexRouter = require('./routes/index');
-const usersRouter = require('./routes/users');
-const catalogRouter = require('./routes/catalog'); //Import routes for "catalog" area of site
+app.use(compression()); // Compress all routes
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
-app.use('/catalog', catalogRouter); // Add catalog routes to middleware chain.
+app.use(express.static(path.join(__dirname, "public")));
+
+app.use("/", indexRouter);
+app.use("/users", usersRouter);
+app.use("/catalog", catalogRouter); // Add catalog routes to middleware chain.
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
-	next(createError(404));
+  next(createError(404));
 });
 
 // error handler
 app.use(function (err, req, res, next) {
-	// set locals, only providing error in development
-	res.locals.title = 'Some error title';
-	res.locals.message = err.message;
-	res.locals.error = req.app.get('env') === 'development' ? err : {};
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get("env") === "development" ? err : {};
 
-	// render the error page
-	res.status(err.status || 500);
-	res.render('error');
+  // render the error page
+  res.status(err.status || 500);
+  res.render("error");
 });
 
 module.exports = app;
